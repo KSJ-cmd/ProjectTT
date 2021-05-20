649de1e1e2c7eeea4805a4d9b378b6f698b0d2a8
20181248 이현승 <53048916+CaptainLee-P@users.noreply.github.com>
2021-05-21 00:27:13 +0900

    5/21 project
    *monster tracking player
    *moster attack player

--- TimeT/Assets/Script/Monster.cs
+++ TimeT/Assets/Script/Monster.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class Monster : MonoBehaviour
 {
     public float speed = 10f;
+    private float reversetime = 0f;
+    private float attackspeed = 0.0f;
     private BoxCollider2D MonsterCollider;
     private Rigidbody2D MonsterRigidbody;
+    public GameObject targetPosition;
+    private bool isCamera=false;
+   
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +23,27 @@ public class Monster : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        MonsterRigidbody.velocity=new Vector2(speed, 0);
+
+        reversetime += Time.deltaTime;
+
+        if (isCamera)
+        {
+            Vector2 playervector = new Vector2(targetPosition.transform.position.x, gameObject.transform.position.y);
+            transform.position = Vector2.MoveTowards(gameObject.transform.position, playervector, attackspeed);
+        }
+        else if(reversetime>1)
+        {
+            MonsterRigidbody.velocity = new Vector2(speed, 0);
+            speed *= -1;
+            reversetime = 0f;
+        }
+        Vector2 rayVector = new Vector2(transform.position.x, transform.position.y + 5);
+        RaycastHit2D hit = Physics2D.Raycast(rayVector, transform.forward, 15f);
+        if (hit)
+        {
+            Debug.Log("raycast");
+            StartCoroutine(Attack());
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -27,8 +52,25 @@ public class Monster : MonoBehaviour
         if(collision.tag=="Wall")
         {
             Debug.Log("trigger");
+            reversetime = 0f;
             speed *= -1;
         }
+        if(collision.tag=="MainCamera")
+        {
+            Debug.Log("Camera");
+            isCamera = true;
+        }
     }
+
+    IEnumerator Attack()
+    {
+        MonsterRigidbody.velocity = new Vector2(0, 0);
+        yield return new WaitForSeconds(1);
+        MonsterRigidbody.velocity = new Vector2(speed * 2, 0);
+        yield return new WaitForSeconds(1);
+
+        MonsterRigidbody.velocity = new Vector2(speed, 0);
+    }
+    
 }
 
